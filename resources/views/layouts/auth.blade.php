b91e2a6091a1d254e600cfa6d23f0a56d6f00008
Uj Nana Soekmana <ujex23@gmail.com>
2024-05-17 14:32:34 +0700

    Update

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -7,6 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ isset($title) ? $title : 'Website' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('build/assets/app-B6D7v1_B.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/app-DgjkFXs7.css') }}">
+    <script src="{{ asset('build/assets/app-B6D7v1_B.js') }}"></script>
 </head>
 <body>
 <main class="grow">
