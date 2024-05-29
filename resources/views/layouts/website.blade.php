cbe12ecbd80ebf1d4bde4e4454f8624d0fb3ecaf
Uj Nana Soekmana <ujex23@gmail.com>
2024-05-30 01:35:42 +0700

    Update

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ isset($title) ? $title : 'Website' }}</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{--@vite(['resources/css/app.css', 'resources/js/app.js'])--}}
     <link rel="stylesheet" href="{{ asset('build/assets/app-DgjkFXs7.css') }}">
     <link rel="stylesheet" href="{{ asset('build/assets/app-DWWURUsa.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
